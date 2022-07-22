5563145b0d7782e9f1e3d9940671074a965f2964
Bárbara Daniela Malik <61635416+barbar-a21904946@users.noreply.github.com>
2022-07-22 11:17:52 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,26 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
-
-import 'package:hive/hive.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:projectunispiritfinalt/screens/Meets/meets_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:projectunispiritfinalt/core/config/providers.dart';
+import 'package:projectunispiritfinalt/core/theme.dart';
+import 'package:projectunispiritfinalt/features/meets/presentation/screens/create_meet_screen.dart';
 import 'package:projectunispiritfinalt/screens/apontamentos_screen.dart';
-import 'package:projectunispiritfinalt/screens/create_meet_screen.dart';
 import 'package:projectunispiritfinalt/screens/explorar.dart';
-import 'package:projectunispiritfinalt/screens/explore/ui/homepage/home_page.dart';
 import 'package:projectunispiritfinalt/screens/home_screen.dart';
 import 'package:projectunispiritfinalt/screens/login2/homepage.dart';
 import 'package:projectunispiritfinalt/screens/login2/login.dart';
-import 'package:projectunispiritfinalt/screens/login_screen.dart';
-
 import 'package:projectunispiritfinalt/screens/perfil_screen.dart';
 import 'package:projectunispiritfinalt/screens/splash_screen.dart';
-import 'package:projectunispiritfinalt/screens/tutorial/onboarding_screen.dart';
-import 'package:projectunispiritfinalt/screens/tutorial3/onbording.dart';
+import 'package:projectunispiritfinalt/screens/userProfile/pages/profile_page.dart';
+import 'package:provider/provider.dart';
 
-
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   //await HiveConfig.start();
   await Firebase.initializeApp();
   //wait HiveConfig.start();
 
-
-
-
-
   runApp(const MyApp());
-
-
-
-
-
 }
 
 class MyApp extends StatelessWidget {
@@ -44,36 +29,32 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      color:  Colors.purple,
-      debugShowCheckedModeBanner: false,
-      title: 'UniSpirit',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        bottomNavigationBarTheme: BottomNavigationBarThemeData(
-          backgroundColor: Colors.purple,
-        )
+    return MultiProvider(
+      providers: unispiritProviders,
+      child: MaterialApp(
+        color: Colors.purple,
+        debugShowCheckedModeBanner: false,
+        title: 'UniSpirit',
+        theme: appTheme,
+        routes: {
+          SplashScreen.route: (context) => const SplashScreen(),
+          HomeScreen.route: (context) => const HomeScreen(),
+          ExplorarScreen.route: (context) => const ExplorarScreen(),
+          CreateMeetScreen.route: (context) => const CreateMeetScreen(),
+          // MeetsScreen.route: (context) => const MeetsScreen(),
+          ApontamentosScreen.route: (context) => const ApontamentosScreen(),
+          ProfilePage.route: (context) => const ProfilePage(),
+          HomePage.route: (context) => const HomePage(),
+
+        },
+
+        home:SplashScreen(),
+        //SplashScreen(),
+        //SplashScreen()
+        //HomePage8(),
+        //SplashScreen(),
+        //LoginPage4(),
       ),
-
-      routes: {
-        SplashScreen.route: (context) => const SplashScreen(),
-        HomeScreen.route: (context) => const HomeScreen(),
-        ExplorarScreen.route: (context) => const ExplorarScreen(),
-        CreateMeetScreen.route: (context)=> const CreateMeetScreen(),
-        MeetsScreen.route: (context) => const MeetsScreen(),
-        ApontamentosScreen.route: (context)=>const ApontamentosScreen(),
-        PerfilScreen.route: (context)=> const PerfilScreen(),
-        HomePage.route: (context)=> const HomePage(),
-        LoginPage4.route: (context)=> LoginPage4(),
-
-
-      },
-
-      home:SplashScreen(),
-      //SplashScreen()
-      //HomePage8(),
-      //SplashScreen(),
-      //LoginPage4(),
     );
   }
 }
